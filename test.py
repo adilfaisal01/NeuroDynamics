5c4680677c656cb768f0404200a3a795592fd982
Adil Faisal <adilfaisal01@proton.me>
2026-06-02 00:30:03 -0400

    test: improve plot readability with labels and legend

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 xx=pd.read_parquet("dataset_doublependulumpts.parquet")
 print(xx.columns)
 
-print(xx['config_id'== 99])
-
-plt.plot(xx[xx['config_id'==99]]['time'],xx[xx['config_id'==99]]['no noise angle pendulum 1'])
-plt.plot(xx[xx['config_id'==99]]['time'],xx[xx['config_id'==99]]['noisy pendulum angle 1'])
+plt.plot(xx[xx['config_id']==99]['time'],xx[xx['config_id']==99]['noisy pendulum angle 1'], label='1')
+plt.plot(xx[xx['config_id']==99]['time'],xx[xx['config_id']==99]['no noise angle pendulum 1'],label='2')
+plt.legend()
 plt.show()
