b8960661faecb348fc8a2581dbc64d65e72ae59a
Adil Faisal <adilfaisal01@proton.me>
2026-06-05 16:00:03 -0400

    test: optimize double pendulum evaluation with hamiltonian loss calculation

--- test.py
+++ test.py
@@ -19,46 +19,27 @@ model_transformer.load_state_dict(torch.load('outputs/model_dlen5000_big.pth',ma
 
 ## loading the dataset
 dataset_inference_test= pd.read_parquet('datasets/dataset_doublependulumpts_finetune.parquet')
-config_groups=dataset_inference_test.groupby('config_id')
-trajectories={cid: group.copy() for cid,group in config_groups}
+h_losses = []
+for cid, group in dataset_inference_test.groupby("config_id"):
+    traj = torch.tensor(group.values, dtype=torch.float32)
+    
+    # Model input
+    x = torch.stack([
+        torch.sin(traj[:, 6]), torch.sin(traj[:, 7]),
+        traj[:, 8], traj[:, 9],
+    ], dim=-1).unsqueeze(0)
+    
+    with torch.no_grad():
+        pred = model_transformer(x).squeeze(0)
+    
+    # H-loss on noiseless states
+    states = traj[:, 10:14].numpy()
+    m1, m2, l1, l2 = pred.tolist()
+    dp = DoublePendulum(m1, m2, l1, l2)
+    H = np.array([dp.hamiltonian(s[0], s[1], s[2], s[3]) for s in states])
+    h_loss = np.var(H) / np.mean(np.abs(H))
+    h_losses.append(h_loss)
 
-traj_ids=list(trajectories.keys())
-train_trajectories=torch.tensor(np.array([trajectories[i] for i in traj_ids]),dtype=torch.float32)
-
-norm_trajectories=[]
-param_storage=[]
-
-for i in range(len(train_trajectories)):
-    angle_pendulum_1_norm=torch.sin(train_trajectories[i][:,6])
-    angle_pendulum_2_norm=torch.sin(train_trajectories[i][:,7])
-    omega_1_norm=torch.sin(train_trajectories[i][:,8])
-    omega_2_norm=torch.sin(train_trajectories[i][:,9])
-    mass_1,mass_2,length_1,length_2=train_trajectories[i][0,1],train_trajectories[i][0,2],train_trajectories[i][0,3],train_trajectories[i][0,4]
-    parameters=torch.tensor([mass_1,mass_2,length_1,length_2])
-    param_storage.append(parameters) 
-    new=torch.stack((angle_pendulum_1_norm,angle_pendulum_2_norm,omega_1_norm,omega_2_norm),dim=-1)
-    norm_trajectories.append(new)
-   
-param_tensor = torch.stack(param_storage, dim=0).to(device=dev)
-norm_trajectories=torch.stack(norm_trajectories,dim=0).to(device=dev)
-class DoublePendulumData(Dataset):
-    def __init__(self, params, trajs):
-        self.params = params
-        self.trajectories = trajs
-    def __len__(self):
-        return len(self.params)
-    def __getitem__(self, idx):
-        return self.params[idx], self.trajectories[idx]
-
-loader = DataLoader(DoublePendulumData(param_tensor, norm_trajectories), batch_size=32)
-
-with torch.no_grad():
-    u=0
-    for t_tensor, traj in loader:
-        pred_params=model_transformer(traj)
-        print(pred_params)
-        u+=1
-        if u>=1:
-            break
+print(f"Mean H-loss over Set B: {np.mean(h_losses):.6e}")        
         
 
