2ac9475a4d81adfc71d74e4adec0a475366e9f48
Adil Faisal <adilfaisal01@proton.me>
2026-06-05 21:13:53 -0400

    updated entrypoint file x3

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ case "$1" in
         ;;
     finetune)
         echo "Starting Stage 2 fine-tuning..."
-        python3 /deeplearningtransformers/runpod_finetune.sh
+        bash /deeplearningtransformers/runpod_finetune.sh
         ;;
     *)
         echo "Starting single training run..."
